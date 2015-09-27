85189d9d46df39ee07481b24d197614da5a65361
thomaslam <tuananhlam95@gmail.com>
2015-09-26 21:38:42 -0400

    move submit function into mainController

--- client/mainController.js
+++ client/mainController.js
@@ -4,6 +4,23 @@ angular.module('imagely', [])
   .controller('MainController', ['$scope', '$location', MainController]);
 
 function MainController($scope, $location) {
-  $scope.stuff = "angular stuff";
+  $scope.json;
+
+  $scope.submit = function() {
+    console.log($('#url-input').val());
+    var url = $('#url-input').val();
+
+    $.ajax({
+      'type': 'GET',
+      'contentType': 'application/json; charset=utf-8',
+      'url': 'https://api.clarifai.com/v1/tag/\?url\=' + url,
+      'headers': {
+        'Authorization': 'Bearer URQPb61EeLHho94TOHzZmthOWwHJRL'
+      }
+    }).then(function(json) {
+      console.log(json);
+      $scope.json = JSON.parse(json);
+    });
+  }
 }
 // })();
